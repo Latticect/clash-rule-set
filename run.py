2fa04050e2319bb50aaf490dffe6aeb6263d948a
Lattice <latticect@gmail.com>
2024-01-20 13:47:49 +0800

    perf: run.py

--- run.py
+++ run.py
@@ -1,82 +1,68 @@
-import re
-from datetime import timezone, timedelta, datetime
+from datetime import timezone, datetime, timedelta
+
 import requests
 import yaml
 
-ad_url = ['https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts',
-          'https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts',
-          'https://raw.githubusercontent.com/Latticect/clash-anti-ad-rule-set/main/add.txt']
-
-white_url = ['https://raw.githubusercontent.com/Goooler/1024_hosts/master/whitelist']
+ad_url = "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt"
+add_url = "https://raw.githubusercontent.com/Latticect/clash-anti-ad-rule-set/main/add.txt"
 
 
-
-ad_master_rule = "https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-clash.yaml"
-white_master_rule = "https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-for-clash.yaml"
-
-
-def downlaod_clash_rule_set(url: str):
-    resp = requests.get(url)
-    # resp = requests.get(url)
-    return resp.text
-
-
-def remove_comments(text: str):
-    lines = text.split("\n")
-    result = "\n"
-    for i in lines:
-        if i.startswith("#"):
-            continue
-        result += i + "\n"
-    return result
+def get_adblock(url) -> str:
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        adblock = res.text
+        return adblock
 
 
-def get_domain(text: str):
-    ip_pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
-    cleaned_text = re.sub(ip_pattern, '', text, flags=re.MULTILINE)
-    cleaned_text = re.sub(r'^white\s+', '', cleaned_text, flags=re.MULTILINE)
-    if(cleaned_text[-1]=='.'):
-        cleaned_text = cleaned_text[:-1]
-    return cleaned_text.strip()
+def str_to_list(adblock: str) -> list:
+    adblock = adblock.split("\n")
+    adblock = [x for x in adblock if x != ""]
+    return adblock
 
 
-def write_to_file(text: str, file_name: str):
+def deal_list(adBlock: list):
+    block = []
+    white = []
+    for i in adBlock:
+        if i.startswith("||"):
+            block.append(i.replace("||", "+.")[:-1])
+        elif i.startswith("@@||"):
+            white.append(i.replace("@@||", "+.")[:-1])
+    add = str_to_list(get_adblock(add_url))
+    for i in add:
+        block.append(f"+.{i}")
+    return [block, white]
+
+
+def make_yaml(data: list):
+    yaml_str = yaml.dump({"payload": data})
+    return yaml_str
+
+
+def write_yaml(yaml_str: str, file_name: str):
     SHA_TZ = timezone(
         timedelta(hours=8),
         name='Asia/Shanghai',
     )
     utc_now = datetime.utcnow().replace(tzinfo=timezone.utc)
     beijing_now = utc_now.astimezone(SHA_TZ)
-
-    with open(file_name, 'w', encoding="utf-8") as f:
-        # text文件头添加当前时间
-        f.write(f"# 自动生成  {beijing_now.strftime('%Y-%m-%d %H:%M:%S')}\n")
-        f.write(text)
+    with open(file_name, "w", encoding="utf-8") as f:
+        f.write(f"# 更新时间：{beijing_now}\n\n{yaml_str}")
 
 
-if __name__ == '__main__':
-    t = ""
-    for i in ad_url:
-        t += "\n" + get_domain(remove_comments(downlaod_clash_rule_set(i)))
-    t = '\n'.join(set(t.splitlines()))
-    # print(t)
+def main():
+    adblock = get_adblock(ad_url)
+    adblock = str_to_list(adblock)
+    block, white = deal_list(adblock)
+    yaml_str = make_yaml(block)
+    write_yaml(yaml_str, "Clash_Rule_Set_AntiAD.yaml")
+    yaml_str = make_yaml(white)
+    write_yaml(yaml_str, "Clash_Rule_Set_White.yaml")
 
-    t2 = downlaod_clash_rule_set(ad_master_rule)
-    data = yaml.safe_load(t2)
-    data['payload'].extend(t.splitlines())
-    data['payload'].remove('360.cn')
-    if '' in data['payload']:
-        data['payload'].remove('')
-    # print(data['payload'])
-    write_to_file(yaml.dump(data), 'Clash_Rule_Set_AntiAD.yaml')
 
-    t = ''
-    for i in white_url:
-        t += get_domain(remove_comments(downlaod_clash_rule_set(i)))
-    t = '\n'.join(set(t.splitlines()))
-    t2 = downlaod_clash_rule_set(white_master_rule)
-    data = yaml.safe_load(t2)
-    data['payload'].extend(t.splitlines())
-    if '' in data['payload']:
-        data['payload'].remove('')
-    write_to_file(yaml.dump(data), 'Clash_Rule_Set_White.yaml')
+if __name__ == "__main__":
+    main()
